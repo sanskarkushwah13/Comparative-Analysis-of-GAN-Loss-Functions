bd53a535b0dc0730f0f3b63273c37ff92d7467e8
sanskarkushwah13a <sanskarkushwah13@gmail.com>
2026-06-13 00:11:55 +0530

    yup

--- test_c1.py
+++ test_c1.py
@@ -217,8 +217,6 @@ class InceptionFeatureExtractor(nn.Module):
             nn.AdaptiveAvgPool2d((1, 1))
         )
 
-
-
     def forward(self, x):
         x = nn.functional.interpolate(x, size=(299, 299),
                                        mode='bilinear', align_corners=False)
